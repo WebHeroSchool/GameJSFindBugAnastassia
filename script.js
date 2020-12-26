9f2f7499d7480d05bf3c829fdcd6a39620de3a3b
Anastassia Varabei <nastenkaok1984@gmail.com>
2020-12-26 15:57:14 +0300

    added level

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 const optionsLevel = document.querySelectorAll("options__level");
-const buttonStart = document.querySelector("button_start");
+const button = document.getElementById("button");
 const pole = document.getElementById("pole");
+let cards = 3;
 let countClick = 0;
 
 // Выбор уровня
 let onClick = function (event) {
-    event.preventDefault();
+    // event.preventDefault();
 
     for (let i = 0; i < optionsLevel.length; i++) {
         optionsLevel[i].classList.remove("options__level__active");
     }
+
+    // event.currentTarget.classList.add('options__level__active');
+
+    // console.log('options__level is clicked');
+    // options__level.onClick = event;
+
+    let selectLevel = document.querySelector("options__level__active").getAttribute("id");
+    switch (selectLevel) {
+        case "easy":
+            cards = 3;
+            break;
+
+        case "medium":
+            cards = 6;
+            break;
+
+        case "hard":
+            cards = 10;
+            break;
+    }
+
+};
+
+for (let i = 0; i < optionsLevel.length; i++) {
+    optionsLevel[i].addEventListener('click', onClick, false);
 }
+
+console.log(onClick);
+
+
 
